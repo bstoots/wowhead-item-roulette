f081aa7809c5cd0b4d459867ea45e615e2606b9f
Brian Stoots <bstoots@gmail.com>
2017-12-03 19:58:10 -0500

    Be consistent with the project naming: Wowhead Item Roulette

--- resources/views/roulette.php
+++ resources/views/roulette.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Wowhead Roulette</title>
+    <title>Wowhead Item Roulette</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -75,7 +75,7 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="/">Wowhead Roulette</a>
+      <a class="navbar-brand" href="/">Wowhead Item Roulette</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
